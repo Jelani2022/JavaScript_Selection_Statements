47727c0170af94facdec9cdb15077260b0535bf8
Jelani2022 <jtwash2@gmail.com>
2022-06-08 13:08:35 -0400

    ready to submit

--- app.js
+++ app.js
@@ -72,40 +72,87 @@ let type = "";
 let color = "";
 let size = "";
 
-if (typeId == "01") {
-  type = "Tank top";
-} else if (typeId == "02") {
-  type = "T-Shirt";
-} else if (typeId == "03") {
-  type = "Long Sleeve";
-} else if (typeId == "04") {
-  type == "Sweat Shirt";
-} else {
-  type = "Other";
+//if (typeId == "01") {
+//  type = "Tank top";
+//} else if (typeId == "02") {
+//  type = "T-Shirt";
+//} else if (typeId == "03") {
+//  type = "Long Sleeve";
+//} else if (typeId == "04") {
+//  type == "Sweat Shirt";
+//} else {
+//  type = "Other";
+//}
+switch (typeId){
+    case "01":
+        type = "tank top";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Long Sleeve";
+        break;
+    case "04":
+        type = "Sweat Shirt";
+        break;
+    default:
+        type = "Other";
+    
+}
+//if (colorId == "BL") {
+  //color = "Black";
+//} else if (colorId == "BL") {
+  //color = "Blue";
+//} else if (colorId == "RD") {
+  //color = "Red";
+//} else if (colorId == "PU") {
+  //color = "Purple";
+//} else {
+  //color = "White";
+//}
+switch (colorId){
+    case "BL":
+        color = "Black";
+        break;
+    case "BU":
+        color = "Blue";
+        break;
+    case "RD":
+        color = "Red";
+        break;
+    case "PU":
+        color = "Purple";
+        break;
+    default:
+        color = "White"
 }
 
-if (colorId == "BL") {
-  color = "Black";
-} else if (colorId == "BL") {
-  color = "Blue";
-} else if (colorId == "RD") {
-  color = "Red";
-} else if (colorId == "PU") {
-  color = "Purple";
-} else {
-  color = "White";
+//if (sizeId == "S") {
+//  size = "Small";
+//} else if (sizeId == "M") {
+//  size = "Medium";
+//} else if (sizeId == "L") {
+//  size = "Large";
+//} else if (sizeId == "XL") {
+// size = "Extra Large";
+//} else {
+//  size = "One Size Fits All";
+//}
+switch (sizeId){
+    case "s":
+        size = "small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    case "XL":
+        size = "Extra Large"
+        break;
+    default:
+        size = "One Size Fits All"
 }
-
-if (sizeId == "S") {
-  size = "Small";
-} else if (sizeId == "M") {
-  size = "Medium";
-} else if (sizeId == "L") {
-  size = "Large";
-} else if (sizeId == "XL") {
-  size = "Extra Large";
-} else {
-  size = "One Size Fits All";
-}
-
 console.log(`Product: ${size} ${color} ${type}`);
